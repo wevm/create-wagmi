dbacc15a0019993794df824fa8fc1a72f08d542e
awkweb <tom@meagher.co>
2022-11-12 00:27:24 -0500

    refactor: logging style, validation & install in bg (#3)
    * feat: notify about updates
    
    * Update src/index.ts
    
    Co-authored-by: jxom <jakemoxey@gmail.com>
    
    * Update src/index.ts
    
    Co-authored-by: jxom <jakemoxey@gmail.com>
    
    * Update src/index.ts
    
    Co-authored-by: jxom <jakemoxey@gmail.com>
    
    * Update src/index.ts
    
    Co-authored-by: jxom <jakemoxey@gmail.com>
    
    * refactor: consolidate validation
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ src/utils/notifyUpdate.ts
@@ -0,0 +1,37 @@
+import pico from 'picocolors'
+import checkForUpdate from 'update-check'
+
+import { CLIOptions } from '..'
+
+import packageJson from '../../package.json'
+import { getPackageManager } from './getPackageManager'
+
+const log = console.log
+
+export async function notifyUpdate({ options }: { options: CLIOptions }) {
+  try {
+    const res = await checkForUpdate(packageJson)
+    if (res?.latest) {
+      const packageManager = await getPackageManager({ options })
+      const updateMessage =
+        packageManager === 'pnpm'
+          ? 'pnpm add -g create-wagmi'
+          : packageManager === 'yarn'
+          ? 'yarn global add create-wagmi'
+          : 'npm i -g create-wagmi'
+
+      log(
+        pico.bold(
+          pico.yellow('A new version of `create-wagmi` is available!') +
+            '\n' +
+            'You can update by running: ' +
+            pico.cyan(updateMessage) +
+            '\n',
+        ),
+      )
+    }
+    process.exit()
+  } catch {
+    // ignore error
+  }
+}
--- src/utils/validatePackageName.ts
+++
@@ -1,9 +0,0 @@
-import validateNpmPackageName from 'validate-npm-package-name'
-
-export const validatePackageName = (projectName: string) => {
-  const { validForNewPackages, warnings } = validateNpmPackageName(projectName)
-  return {
-    valid: validForNewPackages,
-    warnings,
-  }
-}
