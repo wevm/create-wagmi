7579ad2091485e67c8302e8d1da1e68d2fe6b9ca
moxey.eth <jakemoxey@gmail.com>
2022-11-25 11:16:18 +1100

    refactor: web3modal

--- templates/vite-react/connectkit/_meta.ts
+++ templates/vite-react/connectkit/_meta.ts
@@ -4,7 +4,6 @@ import { injectProviders } from '../../../src/hooks/connectkit'
 import { createTemplate } from '../../../src/utils'
 
 export default createTemplate({
-  default: true,
   title: 'ConnectKit',
   description: 'Vite (React) wagmi project with ConnectKit included',
   hooks: compose([
--- templates/vite-react/rainbowkit/_meta.ts
+++ templates/vite-react/rainbowkit/_meta.ts
@@ -2,7 +2,6 @@ import { selectAndInjectProviders } from '../../../src/hooks/common'
 import { createTemplate } from '../../../src/utils'
 
 export default createTemplate({
-  default: true,
   title: 'RainbowKit',
   description: 'Vite (React) wagmi project with RainbowKit included',
   hooks: selectAndInjectProviders({ envPrefix: 'VITE_' }),
