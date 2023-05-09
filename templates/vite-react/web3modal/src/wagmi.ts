42334fcd4d11e414cb40f80405f99fd910f4b6dc
moxey.eth <jakemoxey@gmail.com>
2023-05-09 14:50:51 +1000

    chore: update viem

--- templates/vite-react/web3modal/src/wagmi.ts
+++ templates/vite-react/web3modal/src/wagmi.ts
@@ -6,13 +6,11 @@ export const walletConnectProjectId = '<WALLET_CONNECT_PROJECT_ID>'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
-  // @ts-expect-error - TODO: migrate
   [w3mProvider({ projectId: walletConnectProjectId })],
 )
 
 export const config = createConfig({
   autoConnect: true,
-  // @ts-expect-error - TODO: migrate
   connectors: w3mConnectors({
     chains,
     projectId: walletConnectProjectId,
