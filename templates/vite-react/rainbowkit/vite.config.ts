6d0940c80002ba4ec5f4f5903b7790c5d7f85273
Will Cory <willcory10@gmail.com>
2023-09-11 08:14:31 -0700

    Real fix

--- templates/vite-react/rainbowkit/vite.config.ts
+++ templates/vite-react/rainbowkit/vite.config.ts
@@ -3,17 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'globalThis',
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        '@safe-globalThis/safe-apps-provider',
-        '@safe-globalThis/safe-apps-sdk',
-      ],
-    },
-  },
   resolve: {
     alias: {
       process: 'process/browser',
