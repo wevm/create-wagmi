0a49e0e312de18c2ae0fbc422374d200bb91ff8e
Will Cory <willcory10@gmail.com>
2023-09-11 08:15:51 -0700

    Update vite.config.ts

--- templates/vite-react/web3modal/vite.config.ts
+++ templates/vite-react/web3modal/vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'globalThis',
-  },
   resolve: {
     alias: {
       process: 'process/browser',
